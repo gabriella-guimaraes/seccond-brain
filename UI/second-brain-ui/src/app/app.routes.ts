fc060b5866059791aebb97de71b3528719ce2cc4
gabriella-guimaraes <gabigara13@gmail.com>
2024-12-22 21:59:18 -0300

    feat: basic routes created - pages

--- UI/second-brain-ui/src/app/app.routes.ts
+++ UI/second-brain-ui/src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './Pages/home/home.component';
+import { LoginComponent } from './Pages/login/login.component';
+import { SignInComponent } from './Pages/sign-in/sign-in.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: "", component: HomeComponent },
+    { path: "login", component: LoginComponent },
+    { path: "sign-in", component: SignInComponent }
+];
