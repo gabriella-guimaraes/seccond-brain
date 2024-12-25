80ef97cabaa7df88ad6e6dde136dfa2eaa03eb34
gabriella-guimaraes <gabigara13@gmail.com>
2024-12-25 20:57:59 -0300

    feat: User Entity class created - Domain layer

---
+++ API/second-brain-api/second-brain.Domain/Entities/User.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace second_brain.Domain.Entities
+{
+    public class User
+    {
+        public int UserId { get; set; }
+        public string UserName { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public bool IsActive { get; set; }
+    }
+}
