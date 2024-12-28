f4cc7baadcbd5643733829e52c166ba0ad906c49
gabriella-guimaraes <gabigara13@gmail.com>
2024-12-27 23:25:39 -0300

    feat: Login Page created - pages

--- UI/second-brain-ui/src/app/Pages/login/login.component.ts
+++ UI/second-brain-ui/src/app/Pages/login/login.component.ts
@@ -1,12 +1,44 @@
 import { Component } from '@angular/core';
+import {MatIconModule} from '@angular/material/icon';
+import {MatButtonModule} from '@angular/material/button';
+
+import {ChangeDetectionStrategy, signal} from '@angular/core';
+import {takeUntilDestroyed} from '@angular/core/rxjs-interop';
+import {FormControl, FormsModule, ReactiveFormsModule, Validators} from '@angular/forms';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatInputModule} from '@angular/material/input';
+import {merge} from 'rxjs';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [],
+  imports: [MatButtonModule, MatIconModule, MatFormFieldModule, MatInputModule, FormsModule, ReactiveFormsModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
 export class LoginComponent {
+  readonly email = new FormControl('', [Validators.required, Validators.email]);
+  errorMessage = signal('');
+  hide = signal(true);
 
+  constructor() {
+    merge(this.email.statusChanges, this.email.valueChanges)
+      .pipe(takeUntilDestroyed())
+      .subscribe(() => this.updateErrorMessage());
+  }
+
+  updateErrorMessage() {
+    if (this.email.hasError('required')) {
+      this.errorMessage.set('You must enter a value');
+    } else if (this.email.hasError('email')) {
+      this.errorMessage.set('Not a valid email');
+    } else {
+      this.errorMessage.set('');
+    }
+  }
+
+  clickEvent(event: MouseEvent) {
+    this.hide.set(!this.hide());
+    event.stopPropagation();
+  }
 }
--- UI/second-brain-ui/src/app/app.routes.ts
+++ UI/second-brain-ui/src/app/app.routes.ts
@@ -4,7 +4,7 @@ import { LoginComponent } from './Pages/login/login.component';
 import { SignInComponent } from './Pages/sign-in/sign-in.component';
 
 export const routes: Routes = [
-    { path: "", component: HomeComponent },
-    { path: "login", component: LoginComponent },
+    { path: "home", component: HomeComponent },
+    { path: "", component: LoginComponent },
     { path: "sign-in", component: SignInComponent }
 ];
