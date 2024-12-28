d7ecdade444610b000231bdf42355bec0a9db649
gabriella-guimaraes <gabigara13@gmail.com>
2024-12-28 20:04:49 -0300

    feat: generic repository created - Infra layer

---
+++ API/second-brain-api/second-brain.Infra.Data/Repositories/RepositoryBase.cs
@@ -0,0 +1,45 @@
+﻿using second_brain.Domain.Interfaces;
+using second_brain.Infra.Data.Context;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace second_brain.Infra.Data.Repositories
+{
+    public class RepositoryBase<TEntity> : IDisposable, IRepositoryBase<TEntity> where TEntity : class
+    {
+        protected readonly second_brain_context Db = new second_brain_context();
+        public void Add(TEntity obj)
+        {
+            Db.Set<TEntity>().Add(obj);
+            Db.SaveChanges();
+        }
+
+        public void Dispose()
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<TEntity> GetAll()
+        {
+            return Db.Set<TEntity>().ToList();
+        }
+
+        public TEntity GetById(int id)
+        {
+            return Db.Set<TEntity>().Find(id);
+        }
+
+        public void Remove(TEntity obj)
+        {
+            Db.Set<TEntity>().Remove(obj);
+            Db.SaveChanges();
+        }
+
+        public void Update(TEntity obj)
+        {
+            Db.Entry(obj).State = System.Data.Entity.EntityState.Modified;
+            Db.SaveChanges();
+        }
+    }
+}
