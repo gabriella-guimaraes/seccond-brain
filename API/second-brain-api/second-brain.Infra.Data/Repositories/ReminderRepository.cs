62c79344a66561a01208f5a1bcfde96fb870b076
gabriella-guimaraes <gabigara13@gmail.com>
2024-12-28 20:15:17 -0300

    feat: Reminder repository created - Infra layer

---
+++ API/second-brain-api/second-brain.Infra.Data/Repositories/ReminderRepository.cs
@@ -0,0 +1,15 @@
+﻿using second_brain.Domain.Entities;
+using second_brain.Domain.Interfaces;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace second_brain.Infra.Data.Repositories
+{
+    public class ReminderRepository : RepositoryBase<Reminder>, IReminderRepository
+    {
+        public IEnumerable<Reminder> SearchByTitle(string title)
+        {
+            return Db.Reminders.Where(r => r.Title == title).ToList();
+        }
+    }
+}
