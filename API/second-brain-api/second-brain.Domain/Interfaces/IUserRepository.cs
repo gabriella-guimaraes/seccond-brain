0cf7f8aec87ec9148c8ffa2a736e7bb6dcf43e98
gabriella-guimaraes <gabigara13@gmail.com>
2024-12-28 19:55:01 -0300

    feat: User Repository created - Domain Layer

---
+++ API/second-brain-api/second-brain.Domain/Interfaces/IUserRepository.cs
@@ -0,0 +1,9 @@
+﻿using second_brain.Domain.Entities;
+using System.Collections.Generic;
+
+namespace second_brain.Domain.Interfaces
+{
+    public interface IUserRepository : IRepositoryBase<User>
+    {
+    }
+}
