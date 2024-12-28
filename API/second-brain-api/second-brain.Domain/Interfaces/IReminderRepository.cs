c0d1f5289fdc211ad6002a90217fd119e77b6424
gabriella-guimaraes <gabigara13@gmail.com>
2024-12-28 20:10:22 -0300

    feat: Reminder repository created - Domain layer

---
+++ API/second-brain-api/second-brain.Domain/Interfaces/IReminderRepository.cs
@@ -0,0 +1,11 @@
+﻿using second_brain.Domain.Entities;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace second_brain.Domain.Interfaces
+{
+    public interface IReminderRepository : IRepositoryBase<Reminder>
+    {
+        IEnumerable<Reminder> SearchByTitle(string title);
+    }
+}
