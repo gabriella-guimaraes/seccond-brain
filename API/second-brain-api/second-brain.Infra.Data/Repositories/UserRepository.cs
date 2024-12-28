4abb540a6c08d2b5b9ee2034cbc622c831bec625
gabriella-guimaraes <gabigara13@gmail.com>
2024-12-28 20:07:33 -0300

    feat: User repository created - Infra layer

---
+++ API/second-brain-api/second-brain.Infra.Data/Repositories/UserRepository.cs
@@ -0,0 +1,10 @@
+﻿using second_brain.Domain.Entities;
+using second_brain.Domain.Interfaces;
+
+namespace second_brain.Infra.Data.Repositories
+{
+    public class UserRepository : RepositoryBase<User>, IUserRepository
+    {
+        //Buscas específicas
+    }
+}
