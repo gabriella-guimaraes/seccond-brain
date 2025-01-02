bac6af180763a64d49066765e3e2c5c7d54d1d3d
gabriella-guimaraes <gabigara13@gmail.com>
2025-01-02 13:23:15 -0300

    feat: Login Form component - Organism

---
+++ UI/second-brain-ui/src/app/Components/Organisms/login-form/login-form.component.ts
@@ -0,0 +1,99 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { signal } from '@angular/core';
+import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
+import {
+  FormControl,
+  FormsModule,
+  ReactiveFormsModule,
+  Validators,
+  FormGroup,
+  FormBuilder,
+} from '@angular/forms';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+import { merge } from 'rxjs';
+
+@Component({
+  selector: 'app-login-form',
+  standalone: true,
+  imports: [
+    CommonModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatButtonModule,
+    MatIconModule,
+    FormsModule,
+    ReactiveFormsModule,
+  ],
+  templateUrl: './login-form.component.html',
+  styleUrls: ['./login-form.component.css'],
+})
+export class LoginFormComponent {
+  form!: FormGroup;
+  readonly email = new FormControl('', [Validators.required, Validators.email]);
+  readonly password = new FormControl('', [
+    Validators.required,
+    Validators.minLength(8),
+    Validators.maxLength(16),
+  ]);
+  emailErrorMessage = signal('');
+  passwordErrorMessage = signal('');
+  hide = signal(true);
+
+  constructor(private formBuilder: FormBuilder) {
+    merge(this.email.statusChanges, this.email.valueChanges)
+      .pipe(takeUntilDestroyed())
+      .subscribe(() => this.updateEmailErrorMessage());
+
+    merge(this.password.statusChanges, this.password.valueChanges)
+      .pipe(takeUntilDestroyed())
+      .subscribe(() => this.updatePasswordErrorMessage());
+  }
+
+  ngOnInit(): void {
+    this.form = this.formBuilder.group({
+      email: this.email,
+      password: this.password,
+    });
+  }
+
+  updateEmailErrorMessage() {
+    if (this.email.hasError('required')) {
+      this.emailErrorMessage.set('You must enter an email');
+    } else if (this.email.hasError('email')) {
+      this.emailErrorMessage.set('Not a valid email');
+    } else {
+      this.emailErrorMessage.set('');
+    }
+  }
+
+  updatePasswordErrorMessage() {
+    if (this.password.hasError('required')) {
+      this.passwordErrorMessage.set('You must enter a password');
+    } else if (this.password.hasError('minlength')) {
+      this.passwordErrorMessage.set('Password must be at least 8 characters long');
+    } else if (this.password.hasError('maxlength')) {
+      this.passwordErrorMessage.set('Password must not exceed 16 characters');
+    } else {
+      this.passwordErrorMessage.set('');
+    }
+  }
+
+  clickEvent(event: MouseEvent) {
+    this.hide.set(!this.hide());
+    event.stopPropagation();
+  }
+
+  onSubmit() {
+    //envio de dados para a API
+    if (this.form.valid) {
+      console.log('Email:', this.form.get('email')?.value);
+      console.log('Password:', this.form.get('password')?.value);
+    } else {
+      console.log('Form is invalid');
+    }
+  }
+}
--- UI/second-brain-ui/src/app/Pages/login/login.component.ts
+++ UI/second-brain-ui/src/app/Pages/login/login.component.ts
@@ -3,45 +3,17 @@ import {MatIconModule} from '@angular/material/icon';
 import {MatButtonModule} from '@angular/material/button';
 import { IntroComponent } from '../../Components/Organisms/intro/intro.component';
 
-import {ChangeDetectionStrategy, signal} from '@angular/core';
-import {takeUntilDestroyed} from '@angular/core/rxjs-interop';
-import {FormControl, FormsModule, ReactiveFormsModule, Validators} from '@angular/forms';
-import {MatFormFieldModule} from '@angular/material/form-field';
-import {MatInputModule} from '@angular/material/input';
-import {merge} from 'rxjs';
 
+import { LoginFormComponent } from '../../Components/Organisms/login-form/login-form.component';
 import { IconButtonComponent } from '../../Components/Atoms/icon-button/icon-button.component';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [MatButtonModule, MatIconModule, MatFormFieldModule, MatInputModule, FormsModule, ReactiveFormsModule, IconButtonComponent, IntroComponent],
+  imports: [MatButtonModule, MatIconModule, IconButtonComponent, IntroComponent, LoginFormComponent],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
 export class LoginComponent {
-  readonly email = new FormControl('', [Validators.required, Validators.email]);
-  errorMessage = signal('');
-  hide = signal(true);
-
-  constructor() {
-    merge(this.email.statusChanges, this.email.valueChanges)
-      .pipe(takeUntilDestroyed())
-      .subscribe(() => this.updateErrorMessage());
-  }
-
-  updateErrorMessage() {
-    if (this.email.hasError('required')) {
-      this.errorMessage.set('You must enter a value');
-    } else if (this.email.hasError('email')) {
-      this.errorMessage.set('Not a valid email');
-    } else {
-      this.errorMessage.set('');
-    }
-  }
-
-  clickEvent(event: MouseEvent) {
-    this.hide.set(!this.hide());
-    event.stopPropagation();
-  }
+  
 }
