210957a7ea26a6da518397e09c3e34c5e5367697
gabriella-guimaraes <gabigara13@gmail.com>
2025-01-05 22:16:31 -0300

    feat: animations

--- UI/second-brain-ui/src/app/Components/Organisms/intro/intro.component.ts
+++ UI/second-brain-ui/src/app/Components/Organisms/intro/intro.component.ts
@@ -1,13 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, HostListener, ElementRef } from '@angular/core';
 import { IconButtonComponent } from '../../Atoms/icon-button/icon-button.component';
+import { ObserveVisibilityDirective } from '../../../utils/observe-visibility.directive';
+import { rightFloatAnimation } from '../../../utils/animations';
 
 @Component({
   selector: 'app-intro',
   standalone: true,
-  imports: [IconButtonComponent],
+  imports: [IconButtonComponent, ObserveVisibilityDirective],
   templateUrl: './intro.component.html',
-  styleUrl: './intro.component.css'
+  styleUrl: './intro.component.css',
+  animations: [rightFloatAnimation]
 })
 export class IntroComponent {
+
+  isVisible: boolean = false;
+
+  constructor(private elementRef: ElementRef) { }
+
+  @HostListener('window:scroll', [])
+  onScroll() {
+    const sectionElement = this.elementRef.nativeElement.querySelector('.content');
+    const rect = sectionElement.getBoundingClientRect();
+    const windowHeight = window.innerHeight;
+    const triggerHeight = windowHeight * 0.5;
+
+    // Verificar se a parte superior do elemento está dentro da janela de visualização
+    this.isVisible = rect.top < triggerHeight;
+  }
 
 }
