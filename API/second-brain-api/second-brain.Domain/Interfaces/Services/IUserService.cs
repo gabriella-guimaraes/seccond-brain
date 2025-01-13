63d07fca260ad391052f3e8cb3686c940bed1cfa
gabriella-guimaraes <gabigara13@gmail.com>
2025-01-12 21:18:18 -0300

    feat: User Service Interface - Domain layer

---
+++ API/second-brain-api/second-brain.Domain/Interfaces/Services/IUserService.cs
@@ -0,0 +1,10 @@
+﻿using second_brain.Domain.Entities;
+using System;
+using System.Collections.Generic;
+
+namespace second_brain.Domain.Interfaces.Services
+{
+    public interface IUserService : IServiceBase<User>
+    {
+    }
+}
