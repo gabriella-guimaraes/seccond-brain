fed8b5808cf33f05316987c49bbaf9ed3c97f5bf
gabriella-guimaraes <gabigara13@gmail.com>
2025-01-12 22:26:19 -0300

    styles: intro component animations

--- UI/second-brain-ui/src/app/Components/Organisms/intro/intro.component.ts
+++ UI/second-brain-ui/src/app/Components/Organisms/intro/intro.component.ts
@@ -1,31 +1,82 @@
-import { Component, HostListener, ElementRef } from '@angular/core';
+import { Component, HostListener, ElementRef, Renderer2, OnInit, OnDestroy, Inject, PLATFORM_ID } from '@angular/core';
+import { isPlatformBrowser } from '@angular/common';
 import { IconButtonComponent } from '../../Atoms/icon-button/icon-button.component';
-import { ObserveVisibilityDirective } from '../../../utils/observe-visibility.directive';
-import { rightFloatAnimation } from '../../../utils/animations';
+import { rightFloatAnimation, floatAnimation } from '../../../utils/animations';
 
 @Component({
   selector: 'app-intro',
   standalone: true,
-  imports: [IconButtonComponent, ObserveVisibilityDirective],
+  imports: [IconButtonComponent],
   templateUrl: './intro.component.html',
   styleUrl: './intro.component.css',
-  animations: [rightFloatAnimation]
+  animations: [rightFloatAnimation, floatAnimation]
 })
-export class IntroComponent {
-
+export class IntroComponent implements OnInit {
   isVisible: boolean = false;
+  isMobile: boolean = false;
+  private observer: IntersectionObserver | null = null;
 
-  constructor(private elementRef: ElementRef) { }
+  constructor(private elementRef: ElementRef, private render: Renderer2, @Inject(PLATFORM_ID) private platformId: Object) {}
 
-  @HostListener('window:scroll', [])
-  onScroll() {
-    const sectionElement = this.elementRef.nativeElement.querySelector('.content');
-    const rect = sectionElement.getBoundingClientRect();
-    const windowHeight = window.innerHeight;
-    const triggerHeight = windowHeight * 0.5;
+  ngOnInit() {
+    this.checkScreenWidth();
 
-    // Verificar se a parte superior do elemento está dentro da janela de visualização
-    this.isVisible = rect.top < triggerHeight;
+    if (isPlatformBrowser(this.platformId)) {
+      this.initIntersectionObserver();
+    }
+  }
+
+  ngOnDestroy() {
+    if (this.observer) {
+      this.observer.disconnect(); // Remove o observador ao destruir o componente
+    }
+  }
+
+  @HostListener('window:resize', ['$event'])
+  onResize(event: any) {
+    this.checkScreenWidth
+  }
+
+  checkScreenWidth() {
+    if (isPlatformBrowser(this.platformId)) {
+      const sectionElement = this.elementRef.nativeElement.querySelector('#animationTrigger');
+      if (window.innerWidth <= 768) {
+        this.render.addClass(sectionElement, 'mobile');
+        this.isMobile = true;
+      } else {
+        this.render.removeClass(sectionElement, 'mobile');
+        this.isMobile = false;
+      }
+    }
+  }
+
+
+  // @HostListener('window:scroll', [])
+  // onWindowScroll() {
+  //   const element = this.elementRef.nativeElement.querySelector('.content');
+  //   const rect = element.getBoundingClientRect();
+  //   const windowHeight = window.innerHeight;
+  //   const triggerHeight = windowHeight * 1; // Defina a porcentagem da altura da janela aqui
+
+  //   // Verificar se a parte superior do elemento está dentro da janela de visualização
+  //   this.isVisible = rect.top < triggerHeight;
+  // }
+
+  initIntersectionObserver() {
+    const element = this.elementRef.nativeElement.querySelector('#animationTrigger');
+
+    this.observer = new IntersectionObserver(
+      (entries) => {
+        entries.forEach((entry) => {
+          this.isVisible = entry.isIntersecting; // Atualiza a visibilidade com base na interseção
+        });
+      },
+      { threshold: 0.5 } // Dispara quando 50% do elemento está visível
+    );
+
+    if (element) {
+      this.observer.observe(element);
+    }
   }
 
 }
--- UI/second-brain-ui/src/app/utils/observe-visibility.directive.spec.ts
+++
@@ -1,8 +0,0 @@
-import { ObserveVisibilityDirective } from './observe-visibility.directive';
-
-describe('ObserveVisibilityDirective', () => {
-  it('should create an instance', () => {
-    const directive = new ObserveVisibilityDirective();
-    expect(directive).toBeTruthy();
-  });
-});
