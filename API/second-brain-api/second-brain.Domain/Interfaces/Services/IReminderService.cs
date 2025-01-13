0a02ae5dff086ba6554e84c986e16f027a816f9d
gabriella-guimaraes <gabigara13@gmail.com>
2025-01-12 21:20:58 -0300

    feat: Reminder service interface - Domain layer

---
+++ API/second-brain-api/second-brain.Domain/Interfaces/Services/IReminderService.cs
@@ -0,0 +1,11 @@
+﻿using second_brain.Domain.Entities;
+using System;
+using System.Collections.Generic;
+
+namespace second_brain.Domain.Interfaces.Services
+{
+    public interface IReminderService : IServiceBase<Reminder>
+    {
+        IEnumerable<Reminder> SearchByTitle(string title);
+    }
+}
